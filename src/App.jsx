b7bdca8aa790131a6566f16c92ce9fb699fe9863
lilovekarma441 <lilovekarma441@gmail.com>
2025-04-08 17:21:41 +0800

    0408a

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import img0408a from
+import img0408a from './assets/img0408/img1.jpg'
+import img0408b from './assets/img0408/img2.jpg'
+function App() {
+    return (
+        <>
+            <div id="container">
 
-
-    function App() {
-        return (
-            <>
                 <h1>高雄親子景點再＋1！「臺鐵機廠」變身全台最大室內遊樂園，30項免費遊樂設施免費玩，2025年底開放！
                 </h1>
                 <div>
-                    <img className="img1" scr={img0408a} alt="" />
-                    <img className="img1" scr={img0408b} alt="" />
+                    <img  src={img0408a} alt="" />
+                    <img  src={img0408b} alt="" />
+                    
                 </div>
-
-
-            </>
-
-        )
-    }
+            </div>
+        </>
+    )
+}
 
 export default App
